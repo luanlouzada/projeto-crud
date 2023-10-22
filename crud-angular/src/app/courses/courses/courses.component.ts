728e4461bda332240572aab87b64f099baecdac2
Luan Louzada <luanlouzadadev@gmail.com>
2023-10-22 08:39:27 -0300

    table

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-End'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
